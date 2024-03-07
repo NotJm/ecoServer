64008721dea6d8d1d3f81e42e6f6b5a694e2a9de
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-03-07 00:41:32 -0600

    Modified EcoServer implements mqtt user-controller device-controller

--- ecoServer.js
+++ ecoServer.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+const mqtt = require("mqtt");
+// Modulos utilizados
 
 const app = express();
-const port = 3000;
+const port = 3100;
+// Configuracion del puerto
 
 // Configurar middleware para analizar el cuerpo de las solicitudes HTTP
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,11 +17,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // URL de conexión a tu base de datos MongoDB Atlas
-// "mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido"
 const mongoUrl = "mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido";
 
+// Cliente MQTT
+const mqttClient = mqtt.connect("mqtt://broker.hivemq.com");
+
+
 // Ruta para recibir datos desde la ESP32
-app.post('/app/data-afnpg/endpoint/EcoNido', async (req, res) => {
+app.post('/deivce', async (req, res) => {
   const data = req.body;
 
   try {
@@ -28,11 +34,24 @@ app.post('/app/data-afnpg/endpoint/EcoNido', async (req, res) => {
 
     // Obtener una referencia a la base de datos y la colección
     const db = client.db("EcoNido");
-    const collection = db.collection("device");
+    const deviceCollection = db.collection("device");
+    const deviceHistoryCollection = db.collection("device");
 
-    // Insertar los datos en la colección
-    await collection.insertOne(data);
-    console.log("Datos insertados en la base de datos");
+    // Antes de isnertar los datos hay que comprobar que no haya un dispositivo existente
+    const { mac } = data;
+    const existsDevice = await deviceCollection.find({ mac: mac }).toArray();
+
+    // Comprobacion de dispositivo
+    if (!Array.isArray(existsDevice)) {
+      // Insertar los datos en la colección si no existe
+      await deviceCollection.insertOne(data);
+    } else {
+      // Actualiza datos si ya existe dispositivo
+      await deviceCollection.updateOne(data);
+    }
+    
+    // Historial de dispositivo
+    await deviceHistoryCollection.insertOne(data);
 
     // Cerrar la conexión
     client.close();
@@ -45,6 +64,57 @@ app.post('/app/data-afnpg/endpoint/EcoNido', async (req, res) => {
     res.status(500).send("Error al conectar a la base de datos");
   }
 });
+
+// Ruta para recibir datos de mi ecoWeb 
+app.get('/user', async (req, res) => {
+  const data = req.query;
+  try {
+    // Conectar  a la base de datos MongoDBAtlas
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexion exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y las colecciones
+    const db = client.db("EcoNido");
+    const userCollection = db.collection("users");
+
+    // Primero se comprueba si no existe el usuario
+    const { username } = data;
+    const exists = await userCollection.find({ username: username }).toArray();
+
+    if (!Array.isArray(exists)) {
+      await userCollection.insertOne(data);
+      res.send("Datos registrado satisfactoriamente");
+    } else {
+      res.send("Los datos enviados ya existen");
+    }
+
+    res.send("Datos recibidos del usuario");
+
+  } catch (error) {
+    console.error("Error al conectar MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  }
+});
+
+// Manejo MQTT peticiones
+const listen = (state, topic) => {
+  const message = state === "ON" ? "ON" : "OFF";
+  mqttClient.publish(topic, message);
+  console.log("Mensaje Enviado Sactifactoriamente");
+}
+
+// Manejo MQTT GET
+app.get("/mqtt", (req, res) => {
+  const { state, topic } = req.query;
+
+  if(!state || (state !== "ON" && state !== "OFF")) {
+    return res.status(400).send("Parametros Invalidos");
+  }
+
+  listen(state, topic);
+
+});
+
 
 // Manejar errores 404 para rutas no encontradas
 app.use((req, res, next) => {
