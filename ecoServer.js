09fdf3539a9629bc5ddb39ab478a17ee581620ca
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-03-06 23:36:41 -0600

    Update name file

---
+++ ecoServer.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Configurar middleware para analizar el cuerpo de las solicitudes HTTP
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configurar CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
+
+// URL de conexión a tu base de datos MongoDB Atlas
+// "mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido"
+const mongoUrl = "mongodb+srv://notjm:tqsjTGz5oWJlOdm2@eco-nido.dbwpny9.mongodb.net/?retryWrites=true&w=majority&appName=Eco-Nido";
+
+// Ruta para recibir datos desde la ESP32
+app.post('/app/data-afnpg/endpoint/EcoNido', async (req, res) => {
+  const data = req.body;
+
+  try {
+    // Conectar a la base de datos MongoDB Atlas
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexión exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y la colección
+    const db = client.db("EcoNido");
+    const collection = db.collection("device");
+
+    // Insertar los datos en la colección
+    await collection.insertOne(data);
+    console.log("Datos insertados en la base de datos");
+
+    // Cerrar la conexión
+    client.close();
+    console.log("Conexión cerrada");
+
+    // Responder a la ESP32 con un mensaje de confirmación
+    res.send("Datos recibidos y guardados en la base de datos");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  }
+});
+
+// Manejar errores 404 para rutas no encontradas
+app.use((req, res, next) => {
+  res.status(404).send("Ruta no encontrada");
+});
+
+// Manejar errores 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Error del servidor');
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
+});
