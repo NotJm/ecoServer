db7765ad8613fe3d7702d4595ae8b8eade9a5cef
JoseMariaGarciaMendoza <20221065@uthh.edu.mx>
2024-03-17 00:02:00 -0600

    states

--- ecoServer.js
+++ ecoServer.js
@@ -416,8 +416,8 @@ app.post('/user/login', async (req, res) => {
     if (!exists) {
       res.status(401).json({ status: false });
     } else {
-      const { permisos, dispositivo } = exists;
-      res.status(200).json({ status: true, tipo: permisos, dispositivo: dispositivo });
+      const { permisos, dispositivo, light, fan } = exists;
+      res.status(200).json({ status: true, tipo: permisos, dispositivo: dispositivo, lightState: light, fanState: fan});
     }
     client.close();
 
